dfafda7e435680023d5506ab334416fea43b7b0a
lerry903 <lerry903@163.com>
2019-01-12 11:52:48 +0800

    feat: 升级summernote到v0.8.11

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/SysUserOnlineController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/SysUserOnlineController.java
@@ -1,27 +1,23 @@
 package com.ruoyi.web.controller.monitor;
 
-import java.util.List;
-
-import com.ruoyi.common.utils.StringUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.base.AjaxResult;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.common.enums.OnlineStatus;
+import com.ruoyi.common.page.TableDataInfo;
+import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.framework.shiro.session.OnlineSession;
 import com.ruoyi.framework.shiro.session.OnlineSessionDAO;
 import com.ruoyi.framework.util.ShiroUtils;
-import com.ruoyi.common.page.TableDataInfo;
-import com.ruoyi.system.domain.SysUserOnline;
-import com.ruoyi.system.service.impl.SysUserOnlineServiceImpl;
 import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.system.domain.SysUserOnline;
+import com.ruoyi.system.service.ISysUserOnlineService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 在线用户监控
@@ -32,12 +28,12 @@ import com.ruoyi.framework.web.base.BaseController;
 @RequestMapping("/monitor/online")
 public class SysUserOnlineController extends BaseController {
 
-    private final SysUserOnlineServiceImpl userOnlineService;
+    private final ISysUserOnlineService userOnlineService;
 
     private final OnlineSessionDAO onlineSessionDAO;
 
     @Autowired
-    public SysUserOnlineController(SysUserOnlineServiceImpl userOnlineService, OnlineSessionDAO onlineSessionDAO) {
+    public SysUserOnlineController(ISysUserOnlineService userOnlineService, OnlineSessionDAO onlineSessionDAO) {
         this.userOnlineService = userOnlineService;
         this.onlineSessionDAO = onlineSessionDAO;
     }
