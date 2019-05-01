f15adb38641203ce32e5b8e6ff8c8c202aa840c5
lerry903 <lerry903@163.com>
2019-05-01 15:14:43 +0800

    refactor: 国际化资源文件调整

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/MessageUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/MessageUtils.java
@@ -1,6 +1,7 @@
 package com.ruoyi.common.utils;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 /**
  * 获取i18n资源文件
@@ -13,10 +14,10 @@ public class MessageUtils {
      *
      * @param code 消息键
      * @param args 参数
-     * @return
+     * @return 国际化翻译值
      */
     public static String message(String code, Object... args) {
         MessageSource messageSource = SpringUtils.getBean(MessageSource.class);
-        return messageSource.getMessage(code, args, null);
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 }
